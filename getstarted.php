cd0c121432e2f72cecac44cfe320a9e958f64bc4
elliotlee <1738961+elliotlee@users.noreply.github.com>
2019-01-29 18:29:14 -0800

    website update

---
+++ getstarted.php
@@ -0,0 +1,115 @@
+
+
+<!DOCTYPE HTML>
+<!--
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Creative Dental Studio - Gallery</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<a href= file:///E:/code/cds/website/cds/index.html> <span class="logo"><img src="images/cdslogoop.svg" style="height:4em" alt="" /></span></a>
+						<h1>Getting Started with Creative Dental Studio</h1>
+						<!--<p></p>-->
+					</header>
+
+						<div id="main">
+
+						<!-- Content -->
+							<section id="content" class="main">
+
+										<h2>Form</h2>
+										
+										<form method="post" action="https://formspree.io/lee.hanearl@gmail.com" method="POST" />
+											<div class="row gtr-uniform">
+												<input type="text" name="_gotcha" style="display:none" />
+												<input type="hidden" name="_subject" value="Website contact - Creative Dental Studio" />
+												<input type="hidden" name="_next" value="//elliotlee.github.io/cds/thanks.html" />
+												<div class="col-6 col-12-xsmall">
+													<input type="text" name="name" id="demo-name" value="" placeholder="Name" />
+												</div>
+												<div class="col-6 col-12-xsmall">
+													<input type="email" name="_replyto" id="demo-email" value="" placeholder="Email" />
+												</div>
+
+												<div class="col-12">
+													<select name="category" id="demo-category">
+														<option value="">- Category -</option>
+														<option value="1">Intro package</option>
+														<option value="1">Question</option>
+														<option value="1">Contact</option>
+														
+													</select>
+												</div>
+												<div class="col-4 col-12-small">
+													<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+													<label for="demo-priority-low">Low</label>
+												</div>
+												<div class="col-4 col-12-small">
+													<input type="radio" id="demo-priority-normal" name="demo-priority">
+													<label for="demo-priority-normal">Normal</label>
+												</div>
+												<div class="col-4 col-12-small">
+													<input type="radio" id="demo-priority-high" name="demo-priority">
+													<label for="demo-priority-high">High</label>
+												</div>
+												<div class="col-6 col-12-small">
+													<input type="checkbox" id="demo-copy" name="demo-copy">
+													<label for="demo-copy">Email me a copy</label>
+												</div>
+												<div class="col-6 col-12-small">
+													<input type="checkbox" id="demo-human" name="demo-human" checked>
+													<label for="demo-human">Not a robot</label>
+												</div>
+												<div class="col-12">
+													<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+												</div>
+												<div class="col-12">
+													<ul class="actions">
+														<li><input type="submit" value="Send Message" class="primary" /></li>
+														<li><input type="reset" value="Reset" /></li>
+													</ul>
+												</div>
+											</div>
+										</form>
+								
+</section>
+</div>
+
+
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section>
+
+						</section>
+					</footer>
+
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
